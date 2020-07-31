f4875041fb066eda9ddd12e4691dd8758a7728ea
JTTJ <JTTJ@Thomass-MacBook-Pro.local>
2020-07-30 20:00:39 -0500

    changed monog connection

--- server.js
+++ server.js
@@ -14,10 +14,13 @@ app.use(express.static('public'));
 require('./routes/api_routes')(app);
 require('./routes/html_routes')(app);
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
-    useMongoClient: true
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
+
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:+${PORT}`);
